87bcab7cc52d85e4b9b85ae54c8fecb3c370a48e
Sumit Chaudhary <digitalcodecorporation@gmail.com>
2020-05-15 09:05:35 +0530

    Notifications

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
-import platform, subprocess
-import sched, time
+import platform
+import subprocess
+import sched
+import time
 s = sched.scheduler(time.monotonic, time.sleep)
 
-def checkConnection(host):
-	option = '-c'
-	if platform.system().lower() == 'windows':
-		option = '-n'
-	process = subprocess.run(['ping',option,'1',host], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
-	if process.returncode == 0:
-		print(f"{host} is Up")
-	else:
-		print(f"{host} is Down")
-		sendNotification(host,'Down')
 
-def sendNotification(host,state):
-	subprocess.Popen(['notify-send', f"{host} is {state}"])
-	return
+def checkConnection(host):
+    option = '-c'
+    if platform.system().lower() == 'windows':
+        option = '-n'
+    process = subprocess.run(
+        ['ping', option, '1', host], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if process.returncode == 0:
+        print(f"{host} is Up")
+        sendNotification(host, 'Up')
+    else:
+        print(f"{host} is Down")
+        sendNotification(host, 'Down')
+
+
+def sendNotification(host, state):
+    subprocess.Popen(['notify-send', f"{host} is {state}"])
+    return
+
 
 def program():
-	sites = []
-	with open("list.txt") as file:
-		for line in file:
-			host = line.strip()
-			checkConnection(host)
-	s.enter(10,1,program)
+    sites = []
+    with open("list.txt") as file:
+        for line in file:
+            host = line.strip()
+            checkConnection(host)
+    s.enter(10, 1, program)
+
 
 if __name__ == '__main__':
-	s.enter(10,1,program)
-	s.run()
+    s.enter(10, 1, program)
+    s.run()
